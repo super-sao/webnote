529be1b5d3b350e95ad5f92c4a2143351669e8b6
luckchaochao <54315474+luckchaochao@users.noreply.github.com>
2020-01-10 21:38:25 +0800

    基础页面

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,75 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/content">About</router-link>
     </div>
-    <router-view/>
+    <router-view/>-->
+    <Content></Content>
   </div>
 </template>
+<script>
+//  引进组件
+import Content from "./views/Content";
+
+export default {
+  //  数据
+  data() {
+    return {};
+  },
+
+  //  接受数据
+  props: {
+    // parentData: {
+    //   type: Object,
+    //   default: () => {}
+    // }
+  },
+
+  //  函数主体
+  methods: {},
+
+  //  计算属性
+  computed: {},
+
+  //  侦听器
+  watch: {},
+
+  //  注册组件
+  components: {
+    Content
+  },
+
+  //  自定义指令
+  directives: {},
+
+  //  过滤器
+  filters: {}
+};
+</script>
+
 
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.clear::after {
+  content: "";
+  display: block;
+  clear: both;
+  visibility: hidden;
+  overflow: hidden;
+  height: 0;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+* {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  color: #000;
+  text-decoration: none;
 }
 </style>
---
+++ src/views/Content.vue
@@ -0,0 +1,452 @@
+<template>
+  <div id="content">
+    <header>
+      <div class="logoWrap">
+        <img src="../assets/fontLogo.png" alt="logo" />
+      </div>
+      <div class="topNav">
+        <ul class="topNavBox">
+          <li
+            @click="change(item)"
+            :class="[topNavType==item?'topNavType':'']"
+            :key="index"
+            v-for="(item,index) in topNav"
+          >
+            {{item}}
+            <!-- <router-link :to="'/leftNav/'+item"></router-link> -->
+          </li>
+        </ul>
+      </div>
+    </header>
+
+    <!-- <router-view class="aaa"></router-view> -->
+    <div class="leftNav">
+      <div class="leftNavBox" v-for="(item, index) in leftNav" :key="index">
+        <h3>{{item[0]}}</h3>
+
+        <div class="leftNavBoxList" v-for="(item, index) in item[1]" :key="index">
+          <span>{{item}}</span>
+        </div>
+      </div>
+    </div>
+    <div class="rightContent"></div>
+  </div>
+</template> 
+
+<script>
+//  引进组件
+// import Content from "./views/Content";
+
+export default {
+  //  数据
+  data() {
+    return {
+      topNav: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "Vue",
+        "React",
+        "WeChatApp",
+        "Node",
+        "Git",
+        "Linux"
+      ],
+      leftNav: [
+        [
+          "基础",
+          [
+            "<!DOCTYPE>",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ],
+        [
+          "格式",
+          [
+            "<!DOCTYPE>",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ],
+        [
+          "表单",
+          [
+            "<!DOCTYPE> ",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ],
+        [
+          "框架",
+          [
+            "<!DOCTYPE> ",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ],
+        [
+          "图像",
+          [
+            "<!DOCTYPE> ",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ],
+        [
+          "音频/视频",
+          [
+            "<!DOCTYPE> ",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ],
+        [
+          "链接",
+          [
+            "<!DOCTYPE> ",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ],
+        [
+          "列表",
+          [
+            "<!DOCTYPE> ",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ],
+        [
+          "表格",
+          [
+            "<!DOCTYPE> ",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ],
+        [
+          "样式/节",
+          [
+            "<!DOCTYPE> ",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ],
+        [
+          "元信息",
+          [
+            "<!DOCTYPE> ",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ],
+        [
+          "编程",
+          [
+            "<!DOCTYPE> ",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ],
+        [
+          "元信息",
+          [
+            "<!DOCTYPE> ",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ],
+        [
+          "元信息",
+          [
+            "<!DOCTYPE> ",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ],
+        [
+          "元信息",
+          [
+            "<!DOCTYPE> ",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ],
+        [
+          "元信息",
+          [
+            "<!DOCTYPE> ",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ],
+        [
+          "元信息",
+          [
+            "<!DOCTYPE> ",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ],
+        [
+          "元信息",
+          [
+            "<!DOCTYPE> ",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ],
+        [
+          "元信息",
+          [
+            "<!DOCTYPE> ",
+            "<html>",
+            "<title>",
+            "<body>",
+            "<h1> to <h6>",
+            "<p>",
+            "<br>",
+            "<hr>",
+            "<!--...-->"
+          ]
+        ]
+      ],
+      topNavType: "HTML",
+      leftNavType: "基础"
+    };
+  },
+
+  //  接受数据
+  props: {
+    // parentData: {
+    //   type: Object,
+    //   default: () => {}
+    // }
+  },
+
+  //  函数主体
+  methods: {
+    // a() {
+    //   window.console.log(localStorage);
+    // }
+    change(topNavType) {
+      // window.console.log(index)
+      this.topNavType = topNavType;
+    }
+  },
+
+  //  计算属性
+  computed: {},
+
+  //  侦听器
+  watch: {
+    "$route.path": function(to, from) {
+      window.console.log(to);
+      window.console.log(from);
+    }
+  },
+
+  //  注册组件
+  components: {
+    // Content
+  },
+
+  //  自定义指令
+  directives: {},
+
+  //  过滤器
+  filters: {}
+};
+</script>
+
+
+<style lang="less">
+@mainColor: #006400;
+@borderColor: #84ceaf;
+#content {
+  & > header {
+    // background-color: #aaa;
+    display: flex;
+    justify-content: space-between;
+    // height: 60px;
+    position: relative;
+    top: 0;
+    border-bottom: 1px solid #ebebeb;
+
+    .logoWrap {
+      img {
+        display: block;
+        height: 40px;
+        margin: 10px 30px;
+        // cursor: pointer;
+      }
+    }
+    .topNavBox {
+      display: flex;
+      li {
+        line-height: 56px;
+        margin-right: 20px;
+        cursor: pointer;
+        border-top: 2px solid transparent;
+        border-bottom: 2px solid transparent;
+      }
+      li:hover {
+        position: relative;
+        left: 2px;
+        border-bottom: 2px solid @mainColor;
+      }
+      .topNavType {
+        border-bottom: 2px solid @mainColor;
+      }
+    }
+  }
+
+  .leftNav {
+    position: absolute;
+    width: 200px;
+    top: 60px;
+    left: 0px;
+    bottom: 0px;
+    overflow-x: hidden;
+    overflow-y: auto;
+    // background-color: #aaa;
+    border-right: 1px solid #F1F1F1;
+    .leftNavBox {
+      margin-right: 40px;
+      margin-left: 20px;
+      overflow: hidden;
+      height: 42px;
+      h3 {
+        margin: 8px 0px;
+        cursor: pointer;
+      }
+      .leftNavBoxList {
+        // padding: 2px;
+        cursor: pointer;
+        span {
+          display: inline-block;
+          padding: 4px 0px 2px 0px;
+          margin-bottom: 2px;
+          border-bottom: 2px solid transparent;
+        }
+        &:hover span {
+          display: inline-block;
+          border-bottom: 2px solid @borderColor;
+        }
+      }
+    }
+    & > .leftNavBox:hover {
+      height: auto;
+    }
+  }
+}
+</style>
